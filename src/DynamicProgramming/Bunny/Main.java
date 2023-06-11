8fca6c12a6910630ad3a4526230d1b9489c16d29
Grovox <Grovox@bk.ru>
2023-06-11 14:30:45 +0300

    DP refactor Bunny

--- src/DynamicProgramming/Bunny/Main.java
+++ src/DynamicProgramming/Bunny/Main.java
@@ -5,22 +5,21 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int N = Integer.parseInt(scanner.nextLine());
         int K = Integer.parseInt(scanner.nextLine());
+        int N = Integer.parseInt(scanner.nextLine());
 
         double[] dp = new double[N + 1];
 
         dp[0] = 1;
         dp[1] = 1;
 
-        int step = 1;
-        for(int i = 2; i <= N; i ++){
-            for(int j = step; j < i; j++){
-                dp[i] += dp[j];
-            }
-            if(i > K){
-                step ++;
-            }
+        for (int i = 2; i <= K; i ++){
+            dp[i] = dp[i - 1] * 2;
+            System.out.println(i + " q1 " + dp[i]);
+        }
+        for (int i = K + 1; i <= N; i++){
+            dp[i] = dp[i - 1] * 2 - dp[i - K - 1];
+            System.out.println(i +" q2 " + dp[i]);
         }
 
         System.out.println(dp[N]);
