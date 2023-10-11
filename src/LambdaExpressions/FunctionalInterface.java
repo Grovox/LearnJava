2dfd60df2655682f02d0b0da610eb9260ca2af57
Grovox <Grovox@bk.ru>
2023-10-11 13:59:37 +0300

    LambdaExpressions.FunctionalInterface

---
+++ src/LambdaExpressions/FunctionalInterface.java
@@ -0,0 +1,7 @@
+package LambdaExpressions;
+// Если интерфейс содержит один абстрактный класс, он называется функциональным
+
+@java.lang.FunctionalInterface
+public interface FunctionalInterface {
+    double getValue();
+}
